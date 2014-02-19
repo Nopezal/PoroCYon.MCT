1f35be7d3fa8f538b7d6207cbd5249106e33f897
PoroCYon <pietersluys2@gmail.com>
2014-02-19 17:17:44 +0100

    MessageBox.TextIsCopyable

--- tAPI SDK/GUI/MessageBox.cs
+++ tAPI SDK/GUI/MessageBox.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Windows.Forms;
 using Microsoft.Xna.Framework;
 using TAPI.SDK.GUI.Controls;
+using TAPI.SDK.Input;
 
 namespace TAPI.SDK.GUI
 {
@@ -24,6 +26,11 @@ namespace TAPI.SDK.GUI
         Action<MessageBox, bool?> Result = (mb, res) => { };
 
         /// <summary>
+        /// <param name="copyable">Wether the text of the message box can be copied to the clipboard or not</param>
+        /// </summary>
+        public bool TextIsCopyable = false;
+
+        /// <summary>
         /// What happens when the message box is closed and the result is returned
         /// </summary>
         public Action<MessageBox, bool?> OnResult
@@ -37,12 +44,24 @@ namespace TAPI.SDK.GUI
         /// <summary>
         /// Creates a new instance of the <see cref="TAPI.SDK.GUI.MessageBox"/> class.
         /// </summary>
+        public MessageBox()
+            : this("Looks like something wanted to get your attention,\nbut forgot to say something...\n,Calling assembly (may be useful): "
+            + Assembly.GetCallingAssembly().FullName, "tAPI SDK: Weird alert: " + Assembly.GetCallingAssembly().FullName, MessageBoxButton.OK)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TAPI.SDK.GUI.MessageBox"/> class.
+        /// </summary>
         /// <param name="text">The text the message box contains</param>
         /// <param name="caption">The caption of the message box</param>
         /// <param name="btn">The button type of the message box</param>
         public MessageBox(string text, string caption, MessageBoxButton btn)
             : base(caption)
         {
+            IsDrawnAfter = true;
+
             OnClosed += (w) =>
             {
                 Result(this, null);
@@ -50,6 +69,13 @@ namespace TAPI.SDK.GUI
 
             TextBlock t = new TextBlock(text);
             AddControl(t);
+
+            if (TextIsCopyable)
+                OnUpdate += (c) =>
+                {
+                    if (GInput.Mouse.Rectangle.Intersects(Hitbox) && GInput.Mouse.Left)
+                        Clipboard.SetText(t.Text);
+                };
 
             switch (btn)
             {
@@ -83,8 +109,9 @@ namespace TAPI.SDK.GUI
         /// <param name="text">The text the message box contains</param>
         /// <param name="caption">The title of the message box</param>
         /// <param name="btn">The type of buttons the message box contains</param>
+        /// <param name="copyable">Wether the text of the message box can be copied to the clipboard or not</param>
         /// <returns>What happens when the message box is closed. Hook to this if you want something to happen when the message box is closed.</returns>
-        public static Action<bool?> Show(string text, string caption = null, MessageBoxButton btn = MessageBoxButton.OK)
+        public static Action<bool?> Show(string text, string caption = null, MessageBoxButton btn = MessageBoxButton.OK, bool copyable = false)
         {
             Assembly a = Assembly.GetCallingAssembly();
             object[] titles = a.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
@@ -92,6 +119,7 @@ namespace TAPI.SDK.GUI
 
             return new MessageBox(text, title, btn)
             {
+                TextIsCopyable = copyable,
                 Scale = new Vector2(500f, 350f),
                 Position = new Vector2(Main.screenWidth / 2f - 250f, Main.screenHeight / 2f - 175f)
             }.Show();
