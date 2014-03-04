f2457650a8f3b3133713a922290386e7296b9046
PoroCYon <pietersluys2@gmail.com>
2014-03-04 23:21:57 +0100

    Update XnaExt, fix installer

--- tAPI SDK Installer/App.xaml.cs
+++ tAPI SDK Installer/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Windows;
 
 namespace TAPI.SDK.Installer
@@ -7,8 +8,12 @@ namespace TAPI.SDK.Installer
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            Resources.MergedDictionaries.Add(LoadComponent(new Uri(
-                "PresentationFramework.AeroLite;V4.0.0.0;31bf3856ad364e35;component\\themes/aerolite.normalcolor.xaml", UriKind.Relative)) as ResourceDictionary);
+            try
+            {
+                Resources.MergedDictionaries.Add(LoadComponent(new Uri(
+                    "PresentationFramework.AeroLite;V4.0.0.0;31bf3856ad364e35;component\\themes/aerolite.normalcolor.xaml", UriKind.Relative)) as ResourceDictionary);
+            }
+            catch { } // assembly not found
 
             base.OnStartup(e);
         }
