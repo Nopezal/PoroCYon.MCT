17849cdd6a35cd98f8a1846722bae6244a9d1cdf
PoroCYon <pietersluys2@gmail.com>
2014-05-02 22:22:12 +0200

    Updated XnaExt

--- Mod Creation Tools/MctConstants.cs
+++ Mod Creation Tools/MctConstants.cs
@@ -12,7 +12,7 @@ namespace PoroCYon.MCT
         /// <summary>
         /// The version of the MCT as a string
         /// </summary>
-        public const string VERSION_STRING = "1.0.2.3";
+        public const string VERSION_STRING = "1.0.2.4";
         /// <summary>
         /// The version of the MCT as a <see cref="System.Version"/>
         /// </summary>
