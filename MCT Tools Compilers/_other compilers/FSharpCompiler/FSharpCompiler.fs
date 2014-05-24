01dbd591b5a639353d238a3da1f560f20fd5c9b4
PoroCYon <pietersluys2@gmail.com>
2014-05-24 13:55:42 +0200

    F# compiler

---
+++ MCT Tools Compilers/_other compilers/FSharpCompiler/FSharpCompiler.fs
@@ -0,0 +1,21 @@
+﻿namespace PoroCYon.MCT.Tools.Compiler.Compilers
+
+open System
+open System.CodeDom.Compiler
+open System.Collections.Generic
+open System.Linq
+open PoroCYon.MCT.Tools.Compiler
+open Microsoft.FSharp.Compiler.CodeDom
+
+type FSharpCompiler() = 
+    inherit CodeDomCompilerHelper()
+
+    let ext  = [| ".fs"; ".fsx"        |]
+    let asm  = [| "FSharp.Core.dll"    |]
+    let name = [| "fsharp"; "f#"; "fs" |]
+
+    override this.FileExtensions               with get() = ext
+    override this.LanguageDependancyAssemblies with get() = asm
+    override this.LanguageNames                with get() = name
+
+    override this.CreateCompiler () = (new FSharpCodeProvider() :> CodeDomProvider)
