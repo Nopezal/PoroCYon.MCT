7fdcb262c43ddcb9c6c0998c18e04f1014f543ad
PoroCYon <pietersluys2@gmail.com>
2014-07-16 13:22:55 +0200

    Changed required tAPI version to r9

--- MCT Installer/MainWindow.xaml.cs
+++ MCT Installer/MainWindow.xaml.cs
@@ -318,8 +318,8 @@ namespace PoroCYon.MCT.Installer
                 return "You do not have tAPI installed";
 
             Assembly a = Assembly.LoadFrom(baseFolder + "\\tAPI.exe"); // no try/catch needed, already checked at previous if-statement
-            if ((uint)a.GetType("TAPI.API").GetField("versionAssembly").GetValue(null) < 6u) // not r6
-                return "You do not have tAPI r6 installed";
+            if ((uint)a.GetType("TAPI.API").GetField("versionAssembly").GetValue(null) < 9u) // not r9
+                return "You do not have tAPI r9 installed";
 
             #region Dictionary<VsVersion, string> asString = new Dictionary<VsVersion, string>()
             Dictionary<VsVersion, string> asString = new Dictionary<VsVersion, string>()
