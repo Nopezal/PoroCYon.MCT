c5dae47628d91c8d3ab3440b42818939c22d9491
PoroCYon <pietersluys2@gmail.com>
2014-09-07 12:33:34 +0200

    1.3.3.1

--- Mod Creation Tools/MctConstants.cs
+++ Mod Creation Tools/MctConstants.cs
@@ -12,7 +12,7 @@ namespace PoroCYon.MCT
         /// <summary>
         /// The version of the MCT as a string
         /// </summary>
-        public const string VERSION_STRING = "1.3.3.0";
+        public const string VERSION_STRING = "1.3.3.1";
         /// <summary>
         /// The version of the MCT as a <see cref="System.Version"/>
         /// </summary>
